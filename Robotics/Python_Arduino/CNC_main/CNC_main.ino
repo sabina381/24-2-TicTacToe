d7e2c9aa6530aded30773f8191e9e72f4b7680d2
sabina381 <sabina2378@ewhain.net>
2025-02-18 17:51:05 +0900

    Update CNC_main.ino

--- Robotics/Python_Arduino/CNC_main/CNC_main.ino
+++ Robotics/Python_Arduino/CNC_main/CNC_main.ino
@@ -6,10 +6,7 @@ Stepper stepper_z(A5, A4, A3, A2);
 
 TicTacToeArtist artist(21, 21, &stepper_x, &stepper_y, &stepper_z);
 
-int current_stage = 0; // 현재 상태를 추적하는 변수
-
-char command;
-int int_command;
+int current_state = 0; // 현재 상태를 추적하는 변수
 
 void setup() {
     Serial.begin(115200); // 시리얼 통신 시작
@@ -18,27 +15,25 @@ void setup() {
 
 void loop() {
     if (Serial.available()) { // 새로운 데이터가 들어왔는지 확인
-        command = Serial.read(); // Python에서 보낸 명령 읽기
-        if (command >= '0' && command <= '8'){ // 만약 숫자(위치)명령인 경우 아스키코드를 정수로 변환
-            int_command = command - '0';
-        }
+        char command = Serial.read(); // Python에서 보낸 명령 읽기
+        int int_command = command;
 
-        switch (current_stage) {
+        switch (current_state) {
             case 0 :
                 switch (command) {
                     case 'S' :
                         artist.drawGameBoard();
                         Serial.println("Done: draw Game board");
-                        current_stage = 0;
+                        current_state = 0;
                         break;
 
                     case 'O' :
-                        current_stage = 1;
+                        current_state = 1;
                         Serial.println("Setting O");
                         break;
 
                     case 'X' :
-                        current_stage = 2;
+                        current_state = 2;
                         Serial.println("Setting X");
                         break;
 
@@ -48,15 +43,17 @@ void loop() {
                 break;
 
             case 1 :
+                int_command = command;
                 artist.drawCircle(int_command);
                 Serial.println("Done: drawCircle");
-                current_stage = 0;
+                current_state = 0;
                 break;
             
             case 2 :
+                int_command = command;
                 artist.drawX(int_command);
                 Serial.println("Done: drawX");
-                current_stage = 0;
+                current_state = 0;
                 break;
 
             default:
